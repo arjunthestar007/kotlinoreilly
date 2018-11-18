9ceaae18cf4933456ef53fb0560f43de5fd34a4d
arjun <arjunthestar007@gmail.com>
2018-11-18 07:38:10 -0800

    variables in kotlin

---
+++ app/src/main/java/com/example/arjun/kotlinoreilly/basics/variable.kt
@@ -0,0 +1,17 @@
+package com.example.arjun.kotlinoreilly.basics
+
+fun main(args: Array<String>) {
+
+    var street_name:String="indira nagar"
+    println(street_name)
+
+    var street_name1="indira nagar"
+    println(street_name1)
+
+    val zip:Int=600063
+    println(zip)
+
+    val zip1=600063
+    println(zip1)
+
+}
