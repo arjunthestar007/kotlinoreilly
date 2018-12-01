afe11f3f0d6ce30c7c4460c01f1712c7c37233b1
arjun <arjunthestar007@gmail.com>
2018-11-30 21:27:09 -0800

    higherorderfunctions

---
+++ app/src/main/java/com/example/arjun/kotlinoreilly/functional/higherorderfunction.kt
@@ -0,0 +1,18 @@
+package com.example.arjun.kotlinoreilly.functional
+
+fun operation(x:Int,y:Int,op:(Int,Int)->Int): Int {
+     return op(x,y)
+}
+fun operation(x:Int,op:(Int)->Unit){
+
+}
+
+fun  route (path:String,vararg  actions:(String,String)->String){
+
+}
+fun sum(x:Int,y:Int)=x+y
+
+
+fun main(args: Array<String>) {
+    println(operation(1,2,::sum))
+}
